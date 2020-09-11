d33f642367986745150be5a2823af1949f4a1942
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-11 16:37:59 +0200

    feat: static colors

---
+++ frontend/src/config/colors.js
@@ -0,0 +1,20 @@
+
+/**
+ * Colors that is used in the
+ * color selector and chat
+ */
+export default {
+    purple: "#9c27b0",
+    red: "#d32f2f",
+    pink: "#d81b60",
+    indigo: "#3949ab",
+    blue: "#1e88e5",
+    teal: "#009688",
+    cyan: "#0097a7",
+    green: "#43a047",
+    lime: "#cddc39",
+    yellow: "#ffeb3b",
+    orange: "#fb8c00",
+    bluegrey: "#607d8b",
+    white: "#ffffff",
+};
